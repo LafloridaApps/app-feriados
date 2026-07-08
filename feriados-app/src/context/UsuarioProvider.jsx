75028011c11290e4b1da9ac68316ab7f7a39d591
Mirko Gutierrez <mirkogutierezaravena@gmail.com>
2026-07-08 13:27:12 -0400

    Refactor: mejora la legibilidad del código y actualiza la verificación de respuesta en UsuarioProvider

--- feriados-app/src/context/UsuarioProvider.jsx
+++ feriados-app/src/context/UsuarioProvider.jsx
@@ -6,7 +6,7 @@ import { UsuarioContext } from "./UsuarioContext";
 export const UsuarioProvider = ({ children }) => {
 	const [funcionario, setFuncionario] = useState(null);
 	const [rut, setRut] = useState(null);
- 
+ /**
 	useEffect(() => {
 		// Check if there is a rut in the URL query parameters
 		const urlParams = new URLSearchParams(globalThis.location.search);
@@ -17,13 +17,13 @@ export const UsuarioProvider = ({ children }) => {
 		}
 	}, []);
 
-
+*/
 
 	useEffect(() => {
 		const getFuncionarioRut = async () => {
 			try {
 				const response = await getFuncionarioApi();
-				if (response && response.rut) {
+				if (response?.rut) {
 					setRut(response.rut);
 				}
 			} catch (error) {
