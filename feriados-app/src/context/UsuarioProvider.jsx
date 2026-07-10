a97f63080577c95d298eae76c6dfdaddd499702e
Mirko Gutierrez <mirkogutierezaravena@gmail.com>
2026-07-10 09:06:37 -0400

    feat: actualiza la URL de la API en .env.production y funcionarioService.js
    para produccion

--- feriados-app/src/context/UsuarioProvider.jsx
+++ feriados-app/src/context/UsuarioProvider.jsx
@@ -6,17 +6,17 @@ import { UsuarioContext } from "./UsuarioContext";
 export const UsuarioProvider = ({ children }) => {
 	const [funcionario, setFuncionario] = useState(null);
 	const [rut, setRut] = useState(null);
- 
-	useEffect(() => {
-		// Check if there is a rut in the URL query parameters
-		const urlParams = new URLSearchParams(globalThis.location.search);
-		const rutFromUrl = urlParams.get('rut');
-
-		if (rutFromUrl) {
-			setRut(rutFromUrl);
-		}
-	}, []);
-
+	/**
+	   useEffect(() => {
+		   // Check if there is a rut in the URL query parameters
+		   const urlParams = new URLSearchParams(globalThis.location.search);
+		   const rutFromUrl = urlParams.get('rut');
+   
+		   if (rutFromUrl) {
+			   setRut(rutFromUrl);
+		   }
+	   }, []);
+	*/
 
 
 	useEffect(() => {
